771d04a16f14a01b6fb3e7a318b6c9d249e44d0d
Kevin Godell <kevin.godell@gmail.com>
2017-10-03 13:54:40 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -9,15 +9,44 @@ function PamDiff(options) {
         return new PamDiff(options);
     }
     Transform.call(this, {objectMode: true});
-    this._rgb2gray = this._parseOptions('rgb2gray', options);
-    this._sensitivity = this._validateNumber(this._parseOptions('sensitivity', options), 30, 1, 255);
-    this._threshold = this._validateNumber(this._parseOptions('threshold', options), 300, 1, 20000);
-    this._newPix = null;
-    this._oldPix = null;
-    this._comparePixels = null;//pixel difference engine, to be determined
+    this.setGrayscale(this._parseOptions('grayscale', options));
+    this.setDifference(this._parseOptions('difference', options));
+    this.setPercent(this._parseOptions('percent', options));
+    this._parseChunk = this._parseFirstChunk;
 }
 
 util.inherits(PamDiff, Transform);
+
+PamDiff.prototype.setGrayscale = function (value) {
+    switch (value) {
+        case 'red' :
+            this._grayscale = this._redToGray;
+            break;
+        case 'green' :
+            this._grayscale = this._greenToGray;
+            break;
+        case 'blue' :
+            this._grayscale = this._blueToGray;
+            break;
+        case 'desaturation' :
+            this._grayscale = this._desaturationToGray;
+            break;
+        case 'luminosity' :
+            this._grayscale = this._luminosityToGray;
+            break;
+        default :
+            this._grayscale = this._averageToGray;
+    }
+};
+
+PamDiff.prototype.setDifference = function (value) {
+    this._difference = this._validateNumber(value, 5, 1, 255);
+    console.log(`difference ${value}`);
+};
+
+PamDiff.prototype.setPercent = function (value) {
+    this._percent = this._validateNumber(value, 5, 1, 100);
+};
 
 PamDiff.prototype._parseOptions = function (option, options) {
     if (options && options.hasOwnProperty(option)) {
@@ -38,111 +67,146 @@ PamDiff.prototype._validateNumber = function (number, def, low, high) {
     }
 };
 
-PamDiff.prototype._getGrayEngine = function (type) {
-    if (type === 'lightness') {
-        return PamDiff.prototype._lightness;
-    } else if (type === 'luminosity') {
-        return PamDiff.prototype._luminosity;
-    } else {
-        return PamDiff.prototype._average;
-    }
-};
-
 //convert rgb to gray
-PamDiff.prototype._average = function (r, g, b) {
+PamDiff.prototype._averageToGray = function (r, g, b) {
     return (r + g + b) / 3;
 };
 
 //convert rgb to gray
-PamDiff.prototype._lightness = function (r, g, b) {
+PamDiff.prototype._desaturationToGray = function (r, g, b) {
     return (Math.max(r, g, b) + Math.min(r, g, b)) / 2;
 };
 
 //convert rgb to gray
-PamDiff.prototype._luminosity = function (r, g, b) {
-    return 0.299 * r + 0.587 * g + 0.114 * b;
+PamDiff.prototype._luminosityToGray = function (r, g, b) {
+    //return 0.299 * r + 0.587 * g + 0.114 * b;
+    return 0.3 * r + 0.59 * g + 0.11 * b;
+};
+
+//convert rgb to gray
+PamDiff.prototype._redToGray = function (r, g, b) {
+    return r;
+};
+
+//convert rgb to gray
+PamDiff.prototype._greenToGray = function (r, g, b) {
+    return g;
+};
+
+//convert rgb to gray
+PamDiff.prototype._blueToGray = function (r, g, b) {
+    return b;
+};
+
+PamDiff.prototype._blackAndWhitePixelDiff = function (chunk) {
+    this._newPix = chunk.pixels;
+    let diffPix = [];
+    for (let i = 0, x = 0, y = 0; i < this._length; i++, x++) {
+        if (x === this._width) {
+            x = 0;
+            y++;
+        }
+        if (this._oldPix[i] !== this._newPix[i]) {
+            diffPix.push({x: x, y: y});
+        }
+    }
+    let percent = Math.ceil(diffPix.length / this._length * 100);
+    if (percent >= this._percent) {
+        this.emit('diff', {diffPix: diffPix, percent: percent});
+    }
+    this._oldPix = this._newPix;
+};
+
+PamDiff.prototype._grayScalePixelDiff = function (chunk) {
+    this._newPix = chunk.pixels;
+    let diffPix = [];
+    for (let i = 0, x = 0, y = 0; i < this._length; i++, x++) {
+        if (x === this._width) {
+            x = 0;
+            y++;
+        }
+        if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._difference) {
+            diffPix.push({x: x, y: y});
+        }
+    }
+    let percent = Math.ceil(diffPix.length / this._length * 100);
+    if (percent >= this._percent) {
+        this.emit('diff', {diffPix: diffPix, percent: percent});
+    }
+    this._oldPix = this._newPix;
+};
+
+PamDiff.prototype._rgbPixelDiff = function (chunk) {
+    this._newPix = chunk.pixels;
+    let diffPix = [];
+    for (let i = 0, x = 0, y = 0; i < this._length; i+=3, x++) {
+        if (x === this._width) {
+            x = 0;
+            y++;
+        }
+        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._difference) {
+            diffPix.push({x: x, y: y});
+        }
+    }
+    let percent = Math.ceil(diffPix.length / this._length * 100);
+    if (percent >= this._percent) {
+        this.emit('diff', {diffPix: diffPix, percent: percent});
+    }
+    this._oldPix = this._newPix;
+};
+
+PamDiff.prototype._rgbAlphaPixelDiff = function (chunk) {
+    this._newPix = chunk.pixels;
+    let diffPix = [];
+    for (let i = 0, x = 0, y = 0; i < this._length; i+=4, x++) {
+        if (x === this._width) {
+            x = 0;
+            y++;
+        }
+        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._difference) {
+            diffPix.push({x: x, y: y});
+        }
+    }
+    let percent = Math.ceil(diffPix.length / this._length * 100);
+    if (percent >= this._percent) {
+        this.emit('diff', {diffPix: diffPix, percent: percent});
+    }
+    this._oldPix = this._newPix;
+};
+
+PamDiff.prototype._parseFirstChunk = function (chunk) {
+    this._width = parseInt(chunk.width);
+    this._oldPix = chunk.pixels;
+    this._length = this._oldPix.length;
+    switch (chunk.tupltype) {
+        case 'blackandwhite' :
+            this._parseChunk = this._blackAndWhitePixelDiff;
+            break;
+        case 'grayscale' :
+            this._parseChunk = this._grayScalePixelDiff;
+            break;
+        case 'rgb' :
+            this._parseChunk = this._rgbPixelDiff;
+            break;
+        case 'rgb_alpha' :
+            this._parseChunk = this._rgbAlphaPixelDiff;
+            break;
+        default :
+            throw Error(`Unsupported tupltype: ${chunk.tupltype}. Supported tupltypes include grayscale(gray), blackandwhite(monob), rgb(rgb24), and rgb_alpha(rgba).`);
+    }
 };
 
 PamDiff.prototype._transform = function (chunk, encoding, callback) {
-    //console.log('transform', chunk.pixels.length);
-    this._oldPix = this._newPix;
-    this._newPix = chunk.pixels;
-    if (this._oldPix !== null) {
-        this._comparePixels(this._oldPix, this._newPix);
-    } else {
-        let width = parseInt(chunk.width);
-        let length = this._newPix.length;
-        let sensitivity = this._sensitivity;//amount of difference between pixels
-        let threshold = this._threshold;//number of pixels that are different
-        //let height = parseInt(chunk.height);
-        //let maxval = parseInt(chunk.maxval);
-        //this._diffPixels = new Uint8ClampedArray(width * height * 4);//size it once based on rgba output, reuse for each diff
-        switch (chunk.tupltype) {
-            case 'blackandwhite' ://each pixel will only be 1 byte with value 0 or 1, depth is 1
-                this._comparePixels = (oldPix, newPix) => {
-                    let coords = [];
-                    for (let i = 0, x = 0, y = 0, p = 0; i < length; i++, x++, p+=4) {
-                        if (x === width) {
-                            x = 0;
-                            y++;
-                        }
-                        if (oldPix[i] !== newPix[i]) {
-                            coords.push({p: p, x: x, y: y});
-                        }
-                    }
-                    if (coords.length > threshold) {
-                        this.emit('diff', {coords: coords});
-                    }
-                };
-                break;
-            case 'grayscale' ://each pixel will only be 1 byte with value 0 to 255, depth is 1
-                this._comparePixels = (oldPix, newPix) => {
-                    let coords = [];
-                    for (let i = 0, x = 0, y = 0, p = 0; i < length; i++, x++, p+=4) {
-                        if (x === width) {
-                            x = 0;
-                            y++;
-                        }
-                        if (Math.abs(oldPix[i] - newPix[i]) > sensitivity) {
-                            coords.push({p: p, x: x, y: y});
-                        }
-                    }
-                    if (coords.length > threshold) {
-                        this.emit('diff', {coords: coords, percent: Math.round(coords.length/length * 100)});
-                    }
-                };
-                break;
-            case 'rgb' ://each pixel will be 3 bytes with value 0 to 255, depth is 3
-            case 'rgb_alpha' ://each pixel will be 4 bytes with value 0 to 255, depth is 4
-                let depth = parseInt(chunk.depth);
-                let toGray = this._getGrayEngine(this._rgb2gray);
-                this._comparePixels = (oldPix, newPix) => {
-                    let coords = [];
-                    for (let i = 0, x = 0, y = 0, p = 0; i < length; i+=depth, x++, p+=4) {
-                        if (x === width) {
-                            x = 0;
-                            y++;
-                        }
-                        if (Math.abs(toGray(oldPix[i], oldPix[i + 1], oldPix[i + 2]) - toGray(newPix[i], newPix[i + 1], newPix[i + 2])) > sensitivity) {
-                            coords.push({p: p, x: x, y: y});
-                        }
-                    }
-                    if (coords.length > threshold) {
-                        this.emit('diff', {coords: coords});
-                    }
-                };
-                break;
-            default :
-                throw Error(`Unsupported tupltype: ${chunk.tupltype}. Supported tupltypes include grayscale(gray), blackandwhite(monob), rgb(rgb24), and rgb_alpha(rgba).`);
-        }
-    }
+    this._parseChunk(chunk);
     callback();
 };
 
 PamDiff.prototype._flush = function (callback) {
-    this._newPix = null;
-    this._oldPix = null;
-    this._comparePixels = null;
+    delete this._oldPix;
+    delete this._newPix;
+    delete this._width;
+    delete this._length;
+    this._parseChunk = this._parseFirstChunk;
     callback();
 };
 
