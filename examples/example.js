421a3b1af1aacbfb51e18067911e29c28bae2c74
Kevin Godell <kevin.godell@gmail.com>
2017-10-13 11:51:31 -0500

    updated example

--- examples/example.js
+++ examples/example.js
@@ -2,38 +2,42 @@
 'use strict';
 const P2P = require('pipe2pam');
 const PamDiff = require('../index');
-const spawn = require('child_process').spawn;
+const ChildProcess = require('child_process');
+const spawn = ChildProcess.spawn;
+const execFile = ChildProcess.execFile;
 
 const params = [
     '-loglevel',
     'quiet',
-    
+
     /* use hardware acceleration */
-    //'-hwaccel',
-    //'auto', //vda, videotoolbox, none, auto
+    '-hwaccel',
+    'auto', //vda, videotoolbox, none, auto
 
     /* use an artificial video input */
-    '-re',
-    '-f',
-    'lavfi',
-    '-i',
-    'testsrc=size=1920x1080:rate=15',
+    /*'-re',
+     '-f',
+     'lavfi',
+     '-i',
+     'testsrc=size=1920x1080:rate=15',*/
 
     /* use an rtsp ip cam video input */
-    /*'-rtsp_transport',
-    'tcp', //udp, http, tcp
+    '-rtsp_transport',
+    'tcp',
     '-i',
-    'rtsp://192.168.1.22:554/user=admin_password=pass_channel=1_stream=0.sdp',*/
-    
+    'rtsp://192.168.1.4:554/user=admin_password=pass_channel=1_stream=0.sdp',
+
+    /* set output flags */
     '-an',
     '-c:v',
     'pam',
+    '-pix_fmt',
+    'rgb24',//rgba, rgb24, gray
     '-f',
     'image2pipe',
-    '-pix_fmt',
-    'gray',//rgb24, rgba, monob, gray
     '-vf',
-    'fps=1,scale=iw*1/6:ih*1/6',
+    'fps=1,scale=320:180',//1920:1080 scaled down: 400:225, 384:216, 368:207, 352:198, 336:189, 320:180
+    //'fps=1,scale=iw*1/6:ih*1/6',
     '-frames',
     '1000',
     'pipe:1'
@@ -56,15 +60,33 @@ const p2p = new P2P();
 let counter = 0;
 
 p2p.on('pam', function(data) {
-    //you do not have to do anything here if you are just piping this data to pam-diff
-    console.log('received pam', ++counter);
+    //you do not have to listen to this event if you are just piping this data to pam-diff
+    console.log(`received pam ${++counter}`);
 });
 
-const pamDiff = new PamDiff({grayscale: 'average', difference: 4, percent: 5});
+const pamDiff = new PamDiff({grayscale: 'average', difference: 5, percent: 5});
 
 pamDiff.on('diff', function(data) {
-    //further analyze the pixels for regions or trigger motion detection from this event
-    console.log(`${data.diffPix.length} pixels different, ${data.percent}%`);
+    console.log(data);
+    
+    //comment out the following line if you want to use ffmpeg to create a jpeg from the pam image that triggered an image difference event
+    if(true){return;}
+    
+    const date = new Date();
+    let name = `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}_${date.getHours()}-${date.getUTCMinutes()}-${date.getUTCSeconds()}-${date.getUTCMilliseconds()}`;
+    for (const region of data.trigger) {
+        name += `(${region.name}=${region.percent})`;
+    }
+    const jpeg = `${name}.jpeg`;
+    const ff = execFile('ffmpeg', ['-report', '-f', 'pam_pipe', '-c:v', 'pam', '-i', 'pipe:0', '-c:v', 'mjpeg', '-pix_fmt', 'yuvj422p', '-q:v', '1', '-huffman', 'optimal', jpeg]);
+    ff.stdin.end(data.pam);
+    ff.on('exit', function (data) {
+        if (data === 0) {
+            console.log(`FFMPEG clean exit after creating ${jpeg}`);
+        } else {
+            throw new Error('FFMPEG is not working with current parameters');
+        }
+    });
 });
 
 ffmpeg.stdout.pipe(p2p).pipe(pamDiff);
