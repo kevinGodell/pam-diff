5a3459118ba0517bdda8d5610fadc5fce4d2fc36
Kevin Godell <kevin.godell@gmail.com>
2017-10-13 11:57:07 -0500

    added support for region filtering, optimized pixel array looping

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 
 const util = require('util');
 const Transform = require('stream').Transform;
+const PP = require('polygon-points');
 
 function PamDiff(options) {
     if (!(this instanceof PamDiff)) {
         return new PamDiff(options);
     }
     Transform.call(this, {objectMode: true});
-    this.setGrayscale(this._parseOptions('grayscale', options));
-    this.setDifference(this._parseOptions('difference', options));
-    this.setPercent(this._parseOptions('percent', options));
-    this._parseChunk = this._parseFirstChunk;
+    this.setGrayscale(this._parseOptions('grayscale', options));//global option
+    this.setDifference(this._parseOptions('difference', options));//global option, can be overridden per region
+    this.setPercent(this._parseOptions('percent', options));//global option, can be overridden per region
+    this.setRegions(this._parseOptions('regions', options));//can be no regions or a single region or multiple regions. if no regions, all pixels will be compared.
+    this._parseChunk = this._parseFirstChunk;//first parsing will be reading settings and configuring internal pixel reading
+    //todo add option for break on first region so that pixel is not measured in multiple overlapping regions
 }
 
 util.inherits(PamDiff, Transform);
@@ -40,11 +43,42 @@ PamDiff.prototype.setGrayscale = function (value) {
 };
 
 PamDiff.prototype.setDifference = function (value) {
-    this._difference = this._validateNumber(value, 5, 1, 255);
+    this._difference = this._validateNumber(parseInt(value), 5, 1, 255);
 };
 
 PamDiff.prototype.setPercent = function (value) {
-    this._percent = this._validateNumber(value, 5, 1, 100);
+    this._percent = this._validateNumber(parseInt(value), 5, 1, 100);
+};
+
+PamDiff.prototype.setRegions = function (regions) {
+    if (!regions) {
+        if (this._regions) {
+            delete this._regions;
+            delete this._regionsLength;
+        }
+        this._diffs = 0;
+        return;
+    } else if (!Array.isArray(regions) || regions.length < 1) {
+        throw new Error(`Regions must be an array of at least 1 region object {name: 'region1', difference: 10, percent: 10, polygon: [[0, 0], [0, 50], [50, 50], [50, 0]]}`);
+    }
+    this._regions = [];
+    for (const region of regions) {
+        if (!region.hasOwnProperty('name') || !region.hasOwnProperty('polygon')) {
+            throw new Error('Region must include a name and a polygon property');
+        }
+        const polygonPoints = new PP(region.polygon);
+        this._regions.push(
+            {
+                name: region.name,
+                polygon: polygonPoints,
+                pointsLength: polygonPoints.pointsLength(),
+                difference: this._validateNumber(parseInt(region.difference), this._difference, 1, 255),
+                percent: this._validateNumber(parseInt(region.percent), this._percent, 1, 100),
+                diffs: 0
+            }
+        );
+    }
+    this._regionsLength = this._regions.length;
 };
 
 PamDiff.prototype._parseOptions = function (option, options) {
@@ -99,82 +133,175 @@ PamDiff.prototype._blueToGray = function (r, g, b) {
 
 PamDiff.prototype._blackAndWhitePixelDiff = function (chunk) {
     this._newPix = chunk.pixels;
-    let diffPix = [];
-    for (let i = 0, x = 0, y = 0; i < this._length; i++, x++) {
-        if (x === this._width) {
-            x = 0;
-            y++;
-        }
-        if (this._oldPix[i] !== this._newPix[i]) {
-            diffPix.push([x, y]);
+    for (let y = 0, i = 0; y < this._height; y++) {
+        for (let x = 0; x < this._width; x++, i++) {
+            if (this._regions) {
+                for (let j = 0; j < this._regionsLength; j++) {
+                    if (this._regions[j].polygon.containsPoint([x,y]) === true) {
+                        if (this._oldPix[i] !== this._newPix[i]) {
+                            this._regions[j].diffs++;
+                        }
+                        break;//todo add option for break on first region
+                    }
+                }
+            } else {
+                if (this._oldPix[i] !== this._newPix[i]) {
+                    this._diffs++;
+                }
+            }
         }
     }
-    let percent = Math.ceil(diffPix.length / this._length * 100);
-    if (percent >= this._percent) {
-        this.emit('diff', {diffPix: diffPix, percent: percent});
+    if (this._regions) {
+        const regionDiffArray = [];
+        for (let i = 0; i < this._regionsLength; i++) {
+            const percent = Math.ceil(this._regions[i].diffs / this._regions[i].pointsLength * 100);
+            if (percent >= this._regions[i].percent) {
+                regionDiffArray.push({name: this._regions[i].name, percent: percent});
+            }
+            this._regions[i].diffs = 0;
+        }
+        if (regionDiffArray.length > 0) {
+            this.emit('diff', {trigger: regionDiffArray, pam: chunk.pam});
+        }
+    } else {
+        const percent = Math.ceil(this._diffs / this._length * 100);
+        if (percent >= this._percent) {
+            this.emit('diff', {trigger: [{name: 'percent', percent: percent}], pam: chunk.pam});
+        }
+        this._diffs = 0;
     }
     this._oldPix = this._newPix;
 };
 
 PamDiff.prototype._grayScalePixelDiff = function (chunk) {
     this._newPix = chunk.pixels;
-    let diffPix = [];
-    for (let i = 0, x = 0, y = 0; i < this._length; i++, x++) {
-        if (x === this._width) {
-            x = 0;
-            y++;
-        }
-        if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._difference) {
-            diffPix.push([x, y]);
+    for (let y = 0, i = 0; y < this._height; y++) {
+        for (let x = 0; x < this._width; x++, i++) {
+            if (this._regions) {
+                for (let j = 0; j < this._regionsLength; j++) {
+                    if (this._regions[j].polygon.containsPoint([x,y]) === true) {
+                        if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._regions[j].difference) {
+                            this._regions[j].diffs++;
+                        }
+                        break;//todo add option for break on first region
+                    }
+                }
+            } else {
+                if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._difference) {
+                    this._diffs++;
+                }
+            }
         }
     }
-    let percent = Math.ceil(diffPix.length / this._length * 100);
-    if (percent >= this._percent) {
-        this.emit('diff', {diffPix: diffPix, percent: percent});
+    if (this._regions) {
+        const regionDiffArray = [];
+        for (let i = 0; i < this._regionsLength; i++) {
+            const percent = Math.ceil(this._regions[i].diffs / this._regions[i].pointsLength * 100);
+            if (percent >= this._regions[i].percent) {
+                regionDiffArray.push({name: this._regions[i].name, percent: percent});
+            }
+            this._regions[i].diffs = 0;
+        }
+        if (regionDiffArray.length > 0) {
+            this.emit('diff', {trigger: regionDiffArray, pam: chunk.pam});
+        }
+    } else {
+        const percent = Math.ceil(this._diffs / this._length * 100);
+        if (percent >= this._percent) {
+            this.emit('diff', {trigger: [{name: 'percent', percent: percent}], pam: chunk.pam});
+        }
+        this._diffs = 0;
     }
     this._oldPix = this._newPix;
 };
 
 PamDiff.prototype._rgbPixelDiff = function (chunk) {
     this._newPix = chunk.pixels;
-    let diffPix = [];
-    for (let i = 0, x = 0, y = 0; i < this._length; i+=3, x++) {
-        if (x === this._width) {
-            x = 0;
-            y++;
-        }
-        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._difference) {
-            diffPix.push([x, y]);
+    for (let y = 0, i = 0; y < this._height; y++) {
+        for (let x = 0; x < this._width; x++, i += 3) {
+            if (this._regions) {
+                for (let j = 0; j < this._regionsLength; j++) {
+                    if (this._regions[j].polygon.containsPoint([x,y]) === true) {
+                        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._regions[j].difference) {
+                            this._regions[j].diffs++;
+                        }
+                        break;//todo add option for break on first region
+                    }
+                }
+            } else {
+                if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._difference) {
+                    this._diffs++;
+                }
+            }
         }
     }
-    let percent = Math.ceil(diffPix.length / this._length * 100);
-    if (percent >= this._percent) {
-        this.emit('diff', {diffPix: diffPix, percent: percent});
+    if (this._regions) {
+        const regionDiffArray = [];
+        for (let i = 0; i < this._regionsLength; i++) {
+            const percent = Math.ceil(this._regions[i].diffs / this._regions[i].pointsLength * 100);
+            if (percent >= this._regions[i].percent) {
+                regionDiffArray.push({name: this._regions[i].name, percent: percent});
+            }
+            this._regions[i].diffs = 0;
+        }
+        if (regionDiffArray.length > 0) {
+            this.emit('diff', {trigger: regionDiffArray, pam: chunk.pam});
+        }
+    } else {
+        const percent = Math.ceil(this._diffs / this._length * 100);
+        if (percent >= this._percent) {
+            this.emit('diff', {trigger: [{name: 'percent', percent: percent}], pam: chunk.pam});
+        }
+        this._diffs = 0;
     }
     this._oldPix = this._newPix;
 };
 
 PamDiff.prototype._rgbAlphaPixelDiff = function (chunk) {
     this._newPix = chunk.pixels;
-    let diffPix = [];
-    for (let i = 0, x = 0, y = 0; i < this._length; i+=4, x++) {
-        if (x === this._width) {
-            x = 0;
-            y++;
-        }
-        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._difference) {
-            diffPix.push([x, y]);
+    for (let y = 0, i = 0; y < this._height; y++) {
+        for (let x = 0; x < this._width; x++, i += 4) {
+            if (this._regions) {
+                for (let j = 0; j < this._regionsLength; j++) {
+                    if (this._regions[j].polygon.containsPoint([x,y]) === true) {
+                        if (Math.abs(this._grayscale(this._oldPix[i], this._oldPix[i + 1], this._oldPix[i + 2]) - this._grayscale(this._newPix[i], this._newPix[i + 1], this._newPix[i + 2])) >= this._regions[j].difference) {
+                            this._regions[j].diffs++;
+                        }
+                        break;//todo add option for break on first region
+                    }
+                }
+            } else {
+                if (Math.abs(this._oldPix[i] - this._newPix[i]) >= this._difference) {
+                    this._diffs++;
+                }
+            }
         }
     }
-    let percent = Math.ceil(diffPix.length / this._length * 100);
-    if (percent >= this._percent) {
-        this.emit('diff', {diffPix: diffPix, percent: percent});
+    if (this._regions) {
+        const regionDiffArray = [];
+        for (let i = 0; i < this._regionsLength; i++) {
+            const percent = Math.ceil(this._regions[i].diffs / this._regions[i].pointsLength * 100);
+            if (percent >= this._regions[i].percent) {
+                regionDiffArray.push({name: this._regions[i].name, percent: percent});
+            }
+            this._regions[i].diffs = 0;
+        }
+        if (regionDiffArray.length > 0) {
+            this.emit('diff', {trigger: regionDiffArray, pam: chunk.pam});
+        }
+    } else {
+        const percent = Math.ceil(this._diffs / this._length * 100);
+        if (percent >= this._percent) {
+            this.emit('diff', {trigger: [{name: 'percent', percent: percent}], pam: chunk.pam});
+        }
+        this._diffs = 0;
     }
     this._oldPix = this._newPix;
 };
 
 PamDiff.prototype._parseFirstChunk = function (chunk) {
     this._width = parseInt(chunk.width);
+    this._height = parseInt(chunk.height);
     this._oldPix = chunk.pixels;
     this._length = this._oldPix.length;
     switch (chunk.tupltype) {
@@ -186,9 +313,11 @@ PamDiff.prototype._parseFirstChunk = function (chunk) {
             break;
         case 'rgb' :
             this._parseChunk = this._rgbPixelDiff;
+            //this._increment = 3;//future use
             break;
         case 'rgb_alpha' :
             this._parseChunk = this._rgbAlphaPixelDiff;
+            //this._increment = 4;//future use
             break;
         default :
             throw Error(`Unsupported tupltype: ${chunk.tupltype}. Supported tupltypes include grayscale(gray), blackandwhite(monob), rgb(rgb24), and rgb_alpha(rgba).`);
@@ -210,3 +339,5 @@ PamDiff.prototype._flush = function (callback) {
 };
 
 module.exports = PamDiff;
+//todo get bounding box of all regions combined to exclude some pixels before checking if they exist inside specific regions
+//todo add option for break on first region when coordinate is found
