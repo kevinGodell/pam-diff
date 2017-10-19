5c2f904c66c1c8a4402028a55f4ffa11a2ffbf81
Kevin Godell <kevin.godell@gmail.com>
2017-10-18 20:47:44 -0500

    added test for travis

---
+++ tests/test.js
@@ -0,0 +1,75 @@
+// jshint esversion: 6, globalstrict: true, strict: true
+'use strict';
+
+console.time('=====> testing pam diffs with no region set');
+
+const assert = require('assert');
+
+const P2P = require('pipe2pam');
+
+const PamDiff = require('../index');
+
+const spawn = require('child_process').spawn;
+
+const pamCount = 10;
+
+let pamCounter = 0;
+
+let pamDiffCounter = 0;
+
+const pamDiffResults = [5, 5, 5, 5, 5, 5, 5, 5, 5];
+
+const params = [
+    /* log info to console */
+    '-loglevel',
+    'quiet',
+    '-stats',
+    
+    /* use an artificial video input */
+    '-re',
+    '-f',
+    'lavfi',
+    '-i',
+    'testsrc=size=1920x1080:rate=15',
+
+    /* set output flags */
+    '-an',
+    '-c:v',
+    'pam',
+    '-pix_fmt',
+    'rgb24',
+    '-f',
+    'image2pipe',
+    '-vf',
+    'fps=1,scale=320:180',
+    '-frames',
+    pamCount,
+    'pipe:1'
+];
+
+const p2p = new P2P();
+
+p2p.on('pam', (data) => {
+    pamCounter++;
+});
+
+const pamDiff = new PamDiff({grayscale: 'average', difference: 1, percent: 1});
+
+pamDiff.on('diff', (data) => {
+    assert(data.trigger[0].name === 'percent', 'trigger name is not correct');
+    assert(data.trigger[0].percent === pamDiffResults[pamDiffCounter++], 'trigger percent is not correct');
+});
+
+const ffmpeg = spawn('ffmpeg', params, {stdio: ['ignore', 'pipe', 'inherit']});
+
+ffmpeg.on('error', (error) => {
+    console.log(error);
+});
+
+ffmpeg.on('exit', (code, signal) => {
+    assert(code === 0, `FFMPEG exited with code ${code} and signal ${signal}`);
+    assert(pamDiffCounter === pamCount - 1, `did not get ${pamCount - 1} pam diffs`);
+    console.timeEnd('=====> testing pam diffs with no region set');
+});
+
+ffmpeg.stdout.pipe(p2p).pipe(pamDiff);
