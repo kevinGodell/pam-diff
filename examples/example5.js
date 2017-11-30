a22994bbdcbd886e95d60a493fc18f41b0c35337
Kevin Godell <kevin.godell@gmail.com>
2017-11-29 23:34:30 -0600

    minor change to example5 to prevent create of jpeg from motion data

--- examples/example5.js
+++ examples/example5.js
@@ -86,7 +86,7 @@ pamDiff.on('diff', (data) => {
     console.log(data);
 
     //comment out the following line if you want to use ffmpeg to create a jpeg from the pam image that triggered an image difference event
-    //if(true){return;}
+    if(true){return;}
 
     const date = new Date();
     let name = `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}_${date.getHours()}-${date.getUTCMinutes()}-${date.getUTCSeconds()}-${date.getUTCMilliseconds()}`;
