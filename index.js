e3571c5bc013578077c1e37b5fc9d12ae184be18
Kevin Godell <kevin.godell@gmail.com>
2018-02-05 01:06:05 -0600

    added markup for docs, return this in some methods for chaining

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 'use strict';
 
-const Transform = require('stream').Transform;
+const { Transform } = require('stream');
+
 const PP = require('polygon-points');
 
 class PamDiff extends Transform {
+
+    /**
+     *
+     * @param [options] {Object}
+     * @param [callback] {Function}
+     */
     constructor(options, callback) {
         super(options);
         Transform.call(this, {objectMode: true});
-        this.difference = PamDiff.parseOptions('difference', options);//global option, can be overridden per region
-        this.percent = PamDiff.parseOptions('percent', options);//global option, can be overridden per region
-        this.regions = PamDiff.parseOptions('regions', options);//can be no regions or a single region or multiple regions. if no regions, all pixels will be compared.
+        this.difference = PamDiff._parseOptions('difference', options);//global option, can be overridden per region
+        this.percent = PamDiff._parseOptions('percent', options);//global option, can be overridden per region
+        this.regions = PamDiff._parseOptions('regions', options);//can be no regions or a single region or multiple regions. if no regions, all pixels will be compared.
         this.callback = callback;//callback function to be called when pixel difference is detected
         this._parseChunk = this._parseFirstChunk;//first parsing will be reading settings and configuring internal pixel reading
     }
 
-    //static methods
-
-    static parseOptions(option, options) {
+    /**
+     *
+     * @param option {String}
+     * @param options {Object}
+     * @return {*}
+     * @private
+     */
+    static _parseOptions(option, options) {
         if (options && options.hasOwnProperty(option)) {
             return options[option];
         }
         return null;
     }
 
-    static validateNumber(number, def, low, high) {
+    /**
+     *
+     * @param number
+     * @param def
+     * @param low
+     * @param high
+     * @return {Number}
+     * @private
+     */
+    static _validateNumber(number, def, low, high) {
         if (isNaN(number)) {
             return def;
         } else if (number < low) {
@@ -35,30 +56,71 @@ class PamDiff extends Transform {
         }
     }
 
-    //public methods
-
+    /**
+     *
+     * @deprecated
+     * @param string {String}
+     */
     setGrayscale(string) {
-        console.warn('grayscale option has been removed, averaging has proven to most accurate');
+        console.warn('grayscale option has been removed, "average" has proven to most accurate');
     }
 
+    /**
+     *
+     * @param number {Number}
+     */
     set difference(number) {
-        this._difference = PamDiff.validateNumber(parseInt(number), 5, 1, 255);
+        this._difference = PamDiff._validateNumber(parseInt(number), 5, 1, 255);
     }
 
+    /**
+     *
+     * @return {Number}
+     */
+    get difference() {
+        return this._difference;
+    }
+
+    /**
+     *
+     * @param number {Number}
+     * @return {PamDiff}
+     */
     setDifference(number) {
-        console.warn('setDifference deprecated and will be removed in next minor version, use difference = number');
         this.difference = number;
+        return this;
     }
 
+    /**
+     *
+     * @param number {Number}
+     */
     set percent(number) {
-        this._percent = PamDiff.validateNumber(parseInt(number), 5, 1, 100);
+        this._percent = PamDiff._validateNumber(parseInt(number), 5, 1, 100);
     }
 
+    /**
+     *
+     * @return {Number}
+     */
+    get percent() {
+        return this._percent;
+    }
+
+    /**
+     *
+     * @param number {Number}
+     * @return {PamDiff}
+     */
     setPercent(number) {
-        console.warn('setPercent deprecated and will be removed in next minor version, use difference = number');
         this.percent = number;
+        return this;
     }
 
+    /**
+     *
+     * @param array {Array}
+     */
     set regions(array) {
         if (!array) {
             if (this._regions) {
@@ -67,65 +129,104 @@ class PamDiff extends Transform {
                 delete this._minDiff;
             }
             this._diffs = 0;
-            return;
         } else if (!Array.isArray(array) || array.length < 1) {
             throw new Error(`Regions must be an array of at least 1 region object {name: 'region1', difference: 10, percent: 10, polygon: [[0, 0], [0, 50], [50, 50], [50, 0]]}`);
-        }
-        this._regions = [];
-        this._minDiff = 255;
-        for (const region of array) {
-            if (!region.hasOwnProperty('name') || !region.hasOwnProperty('polygon')) {
-                throw new Error('Region must include a name and a polygon property');
-            }
-            const polygonPoints = new PP(region.polygon);
-            const difference = PamDiff.validateNumber(parseInt(region.difference), this._difference, 1, 255);
-            const percent = PamDiff.validateNumber(parseInt(region.percent), this._percent, 1, 100);
-            this._minDiff = Math.min(this._minDiff, difference);
-            this._regions.push(
-                {
-                    name: region.name,
-                    polygon: polygonPoints,
-                    difference: difference,
-                    percent: percent,
-                    diffs: 0
+        } else {
+            this._regions = [];
+            this._minDiff = 255;
+            for (const region of array) {
+                if (!region.hasOwnProperty('name') || !region.hasOwnProperty('polygon')) {
+                    throw new Error('Region must include a name and a polygon property');
                 }
-            );
-        }
-        this._regionsLength = this._regions.length;
-        this._createPointsInPolygons(this._regions, this._width, this._height);
-    }
-
-    setRegions(array) {
-        console.warn('setRegions deprecated and will be removed in next minor version, use regions = value');
-        this.regions = array;
-    }
-
-    set callback(func) {
-        if (typeof func === 'function') {
-            if (func.length !== 1) {
-                throw new Error('Callback function must only accept 1 argument');
+                const polygonPoints = new PP(region.polygon);
+                const difference = PamDiff._validateNumber(parseInt(region.difference), this._difference, 1, 255);
+                const percent = PamDiff._validateNumber(parseInt(region.percent), this._percent, 1, 100);
+                this._minDiff = Math.min(this._minDiff, difference);
+                this._regions.push(
+                    {
+                        name: region.name,
+                        polygon: polygonPoints,
+                        difference: difference,
+                        percent: percent,
+                        diffs: 0
+                    }
+                );
             }
+            this._regionsLength = this._regions.length;
+            this._createPointsInPolygons(this._regions, this._width, this._height);
+        }
+    }
+
+    /**
+     *
+     * @return {Array}
+     */
+    get regions() {
+        return this._regions;
+    }
+
+    /**
+     *
+     * @param array {Array}
+     * @return {PamDiff}
+     */
+    setRegions(array) {
+        this.regions = array;
+        return this;
+    }
+
+    /**
+     *
+     * @param func {Function}
+     */
+    set callback(func) {
+        if (!func) {
+            delete this._callback;
+        } else if (typeof func === 'function' && func.length === 1) {
             this._callback = func;
         } else {
-            delete this._callback;
+            throw new Error('Callback must be a function that accepts 1 argument.');
         }
     }
 
-    setCallback(func) {
-        console.warn('setCallback deprecated and will be removed in next minor version, use callback = function');
-        this.callback = func;
+    /**
+     *
+     * @return {Function}
+     */
+    get callback() {
+        return this._callback;
     }
 
+    /**
+     *
+     * @param func {Function}
+     * @return {PamDiff}
+     */
+    setCallback(func) {
+        this.callback = func;
+        return this;
+    }
+
+    /**
+     *
+     * @return {PamDiff}
+     */
     resetCache() {
         delete this._oldPix;
         delete this._newPix;
         delete this._width;
         delete this._length;
         this._parseChunk = this._parseFirstChunk;
+        return this;
     }
 
-    //private methods
-
+    /**
+     *
+     * @param regions {Array}
+     * @param width {Number}
+     * @param height {Number}
+     * @private
+     */
     _createPointsInPolygons(regions, width, height) {
         if (regions && width && height) {
             this._pointsInPolygons = [];
@@ -137,6 +238,11 @@ class PamDiff extends Transform {
         }
     }
 
+    /**
+     *
+     * @param chunk
+     * @private
+     */
     _blackAndWhitePixelDiff(chunk) {
         this._newPix = chunk.pixels;
         for (let y = 0, i = 0; y < this._height; y++) {
@@ -195,6 +301,11 @@ class PamDiff extends Transform {
         this._oldPix = this._newPix;
     }
 
+    /**
+     *
+     * @param chunk
+     * @private
+     */
     _grayScalePixelDiff(chunk) {
         this._newPix = chunk.pixels;
         for (let y = 0, i = 0; y < this._height; y++) {
@@ -255,6 +366,11 @@ class PamDiff extends Transform {
         this._oldPix = this._newPix;
     }
 
+    /**
+     *
+     * @param chunk
+     * @private
+     */
     _rgbPixelDiff(chunk) {
         this._newPix = chunk.pixels;
         for (let y = 0, i = 0, p = 0; y < this._height; y++) {
@@ -315,6 +431,11 @@ class PamDiff extends Transform {
         this._oldPix = this._newPix;
     }
 
+    /**
+     *
+     * @param chunk
+     * @private
+     */
     _rgbAlphaPixelDiff(chunk) {
         this._newPix = chunk.pixels;
         for (let y = 0, i = 0, p = 0; y < this._height; y++) {
@@ -375,6 +496,11 @@ class PamDiff extends Transform {
         this._oldPix = this._newPix;
     }
 
+    /**
+     *
+     * @param chunk
+     * @private
+     */
     _parseFirstChunk(chunk) {
         this._width = parseInt(chunk.width);
         this._height = parseInt(chunk.height);
@@ -401,16 +527,34 @@ class PamDiff extends Transform {
         }
     }
 
+    /**
+     *
+     * @param chunk
+     * @param encoding
+     * @param callback
+     * @private
+     */
     _transform(chunk, encoding, callback) {
+        console.time('par');
         this._parseChunk(chunk);
+        console.timeEnd('par');
         callback();
     }
 
+    /**
+     *
+     * @param callback
+     * @private
+     */
     _flush(callback) {
         this.resetCache();
         callback();
     }
 }
 
+/**
+ *
+ * @type {PamDiff}
+ */
 module.exports = PamDiff;
 //todo get bounding box of all regions combined to exclude some pixels before checking if they exist inside specific regions
