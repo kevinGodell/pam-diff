2a3d423a27c379ef4fcd908a086820ee1952153c
Kevin Godell <kevin.godell@gmail.com>
2019-01-03 00:23:27 -0600

    add ignore

--- src/addon.cc
+++ src/addon.cc
@@ -6,9 +6,9 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set(Napi::String::New(env, "grayDiffAllSync"), Napi::Function::New(env, GrayDiffAllSync));
     exports.Set(Napi::String::New(env, "grayDiffAllAsync"), Napi::Function::New(env, GrayDiffAllAsync));
     exports.Set(Napi::String::New(env, "grayDiffMaskSync"), Napi::Function::New(env, GrayDiffMaskSync));
-    //exports.Set(Napi::String::New(env, "grayDiffMaskAsync"), Napi::Function::New(env, GrayDiffMaskAsync));
+    exports.Set(Napi::String::New(env, "grayDiffMaskAsync"), Napi::Function::New(env, GrayDiffMaskAsync));
     exports.Set(Napi::String::New(env, "grayDiffRegionsSync"), Napi::Function::New(env, GrayDiffRegionsSync));
-    //exports.Set(Napi::String::New(env, "grayDiffRegionsAsync"), Napi::Function::New(env, GrayDiffRegionsAsync));
+    exports.Set(Napi::String::New(env, "grayDiffRegionsAsync"), Napi::Function::New(env, GrayDiffRegionsAsync));
     /*exports.Set(Napi::String::New(env, "grayRegionsSync"), Napi::Function::New(env, GrayRegionsSync));
     exports.Set(Napi::String::New(env, "grayRegionsAsync"), Napi::Function::New(env, GrayRegionsAsync));
     exports.Set(Napi::String::New(env, "grayMaskSync"), Napi::Function::New(env, GrayRegionsSync));
