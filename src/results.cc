b0dda1586c0f2c6735c3ba8a79c11df4c0528f29
Kevin Godell <kevin.godell@gmail.com>
2019-03-09 22:42:01 -0600

    dry-ing code

--- src/results.cc
+++ src/results.cc
@@ -37,20 +37,26 @@ ToJs(const Napi::Env &env, const std::vector<BoundsResult> &boundsResultVec, Nap
     }
 }
 
+// all/mask blobs to js
+void
+ToJs(const Napi::Env &env, const BlobsResult &blobsResult, Napi::Array &resultsJs) {
+    SetBlobsResult(env, blobsResult, resultsJs);
+}
+
+// regions blobs to js
+void
+ToJs(const Napi::Env &env, const std::vector<BlobsResult> &blobsResultVec, Napi::Array &resultsJs) {
+    uint_fast32_t j = 0;
+    for (const auto &blobsResult : blobsResultVec) {
+        if (!blobsResult.flagged) continue;
+        SetBlobsResult(env, blobsResult, resultsJs, j++);
+    }
+}
+
 // draw bounding box in gray pixels for all/mask
 void
 DrawGray(const BoundsResult &boundsResult, const uint_fast32_t width, uint_fast8_t *pixels) {
-    uint_fast32_t indexMinY = boundsResult.minY * width;
-    uint_fast32_t indexMaxY = boundsResult.maxY * width;
-    for (uint_fast32_t x = boundsResult.minX; x < boundsResult.maxX; ++x) {
-        pixels[indexMinY + x] = 0x00;
-        pixels[indexMaxY + x] = 0x00;
-    }
-    for (uint_fast32_t y = boundsResult.minY; y < boundsResult.maxY; ++y) {
-        uint_fast32_t indexY = y * width;
-        pixels[indexY + boundsResult.minX] = 0x00;
-        pixels[indexY + boundsResult.maxX] = 0x00;
-    }
+    SetGrayPixels(boundsResult.minX, boundsResult.maxX, boundsResult.minY, boundsResult.maxY, width, pixels);
 }
 
 // draw bounding box in gray pixels for regions
@@ -58,42 +64,14 @@ void
 DrawGray(const std::vector<BoundsResult> &boundsResultVec, const uint_fast32_t width, uint_fast8_t *pixels) {
     for (const auto &boundsResult : boundsResultVec) {
         if (!boundsResult.flagged) continue;
-        uint_fast32_t indexMinY = boundsResult.minY * width;
-        uint_fast32_t indexMaxY = boundsResult.maxY * width;
-        uint_fast8_t *topPtr = &pixels[indexMinY + boundsResult.minX];
-        uint_fast8_t *bottomPtr = &pixels[indexMaxY + boundsResult.minX];
-        uint_fast8_t *leftPtr = &pixels[indexMinY + boundsResult.minX + width];
-        uint_fast8_t *rightPtr = &pixels[indexMinY + boundsResult.maxX + width];
-        for (uint_fast32_t x = boundsResult.minX; x <= boundsResult.maxX; ++x, ++topPtr, ++bottomPtr) {
-            //pixels[indexMinY + x] = 0x00;
-            //pixels[indexMaxY + x] = 0x00;
-            *topPtr = 0x80;
-            *bottomPtr = 0x80;
-        }
-        for (uint_fast32_t y = boundsResult.minY, yLimit = boundsResult.maxY - 1; y < yLimit; ++y, leftPtr += width, rightPtr += width) {
-            //uint_fast32_t indexY = y * width;
-            //pixels[indexY + boundsResult.minX] = 0x00;
-            //pixels[indexY + boundsResult.maxX] = 0x00;
-            *leftPtr = 0x80;
-            *rightPtr = 0x80;
-        }
+        SetGrayPixels(boundsResult.minX, boundsResult.maxX, boundsResult.minY, boundsResult.maxY, width, pixels);
     }
 }
 
 // draw bounding box in rgb pixels for all/mask
 void
 DrawRgb(const BoundsResult &boundsResult, const uint_fast32_t width, const uint_fast32_t pixDepth, uint_fast8_t *pixels) {
-    uint_fast32_t indexMinY = boundsResult.minY * width;
-    uint_fast32_t indexMaxY = boundsResult.maxY * width;
-    for (uint_fast32_t x = boundsResult.minX; x < boundsResult.maxX; ++x) {
-        pixels[(indexMinY + x) * pixDepth] = 0x00;
-        pixels[(indexMaxY + x) * pixDepth] = 0x00;
-    }
-    for (uint_fast32_t y = boundsResult.minY; y < boundsResult.maxY; ++y) {
-        uint_fast32_t indexY = y * width;
-        pixels[(indexY + boundsResult.minX) * pixDepth] = 0x00;
-        pixels[(indexY + boundsResult.maxX) * pixDepth] = 0x00;
-    }
+    SetRgbPixels(boundsResult.minX, boundsResult.maxX, boundsResult.minY, boundsResult.maxY, width, pixDepth, pixels);
 }
 
 // draw bounding box in rgb pixels for regions
@@ -101,74 +79,8 @@ void
 DrawRgb(const std::vector<BoundsResult> &boundsResultVec, const uint_fast32_t width, const uint_fast32_t pixDepth, uint_fast8_t *pixels) {
     for (const auto &boundsResult : boundsResultVec) {
         if (!boundsResult.flagged) continue;
-        uint_fast32_t indexMinY = boundsResult.minY * width;
-        uint_fast32_t indexMaxY = boundsResult.maxY * width;
-        for (uint_fast32_t x = boundsResult.minX; x < boundsResult.maxX; ++x) {
-
-            /*uint_fast8_t *ptr = &pixels[(indexMinY + x) * pixDepth];
-            *ptr = 0xFF;
-            *(++ptr) = 0x00;
-            *(++ptr) = 0x00;
-
-            ptr = &pixels[(indexMaxY + x) * pixDepth];
-            *ptr = 0xFF;
-            *(++ptr) = 0x00;
-            *(++ptr) = 0x00;*/
-
-            pixels[(indexMinY + x) * pixDepth] = 0x00;
-            pixels[(indexMaxY + x) * pixDepth] = 0x00;
-        }
-        for (uint_fast32_t y = boundsResult.minY; y < boundsResult.maxY; ++y) {
-            uint_fast32_t indexY = y * width;
-
-            /*uint_fast8_t *ptr = &pixels[(indexY + boundsResult.minX) * pixDepth];
-            *ptr = 0xFF;
-            *(++ptr) = 0x00;
-            *(++ptr) = 0x00;
-
-            ptr = &pixels[(indexY + boundsResult.maxX) * pixDepth];
-            *ptr = 0xFF;
-            *(++ptr) = 0x00;
-            *(++ptr) = 0x00;*/
-
-
-            pixels[(indexY + boundsResult.minX) * pixDepth] = 0x00;
-            pixels[(indexY + boundsResult.maxX) * pixDepth] = 0x00;
-        }
+        SetRgbPixels(boundsResult.minX, boundsResult.maxX, boundsResult.minY, boundsResult.maxY, width, pixDepth, pixels);
     }
-}
-
-// free memory from heap allocated array used as Buffer data
-void
-DeleteExternalData(Napi::Env /*&env*/, const uint_fast8_t *finalizeData) {
-    delete[] finalizeData;
-}
-
-// all/mask bounds to js
-void
-ToJs(const Napi::Env &env, const BlobsResult &blobsResult, Napi::Array &resultsJs) {
-    Napi::Object obj = Napi::Object::New(env);
-    obj.Set("name", blobsResult.name);
-    obj.Set("percent", blobsResult.percent);
-    obj.Set("minX", blobsResult.minX);
-    obj.Set("maxX", blobsResult.maxX);
-    obj.Set("minY", blobsResult.minY);
-    obj.Set("maxY", blobsResult.maxY);
-    Napi::Array blobsJs = Napi::Array::New(env);
-    uint_fast32_t j = 0;
-    for (const auto &blob : blobsResult.blobs) {
-        if (!blob.flagged) continue;
-        Napi::Object blobJs = Napi::Object::New(env);
-        blobJs.Set("label", blob.label);
-        blobJs.Set("percent", blob.percent);
-        blobJs.Set("minX", blob.minX);
-        blobJs.Set("maxX", blob.maxX);
-        blobJs.Set("minY", blob.minY);
-        blobJs.Set("maxY", blob.maxY);
-        blobsJs.Set(j++, blobJs);
-    }
-    obj.Set("blobs", blobsJs);
-    resultsJs.Set(0u, obj);
 }
 
 // draw blobs bounding box in gray pixels for all/mask
@@ -176,16 +88,12 @@ void
 DrawGray(const BlobsResult &blobsResult, const uint_fast32_t width, uint_fast8_t *pixels) {
     for (const auto &blob : blobsResult.blobs) {
         if (!blob.flagged) continue;
-        uint_fast32_t indexMinY = blob.minY * width;
-        uint_fast32_t indexMaxY = blob.maxY * width;
-        for (uint_fast32_t x = blob.minX; x < blob.maxX; ++x) {
-            pixels[indexMinY + x] = 0x00;
-            pixels[indexMaxY + x] = 0x00;
-        }
-        for (uint_fast32_t y = blob.minY; y < blob.maxY; ++y) {
-            uint_fast32_t indexY = y * width;
-            pixels[indexY + blob.minX] = 0x00;
-            pixels[indexY + blob.maxX] = 0x00;
-        }
+        SetGrayPixels(blob.minX, blob.maxX, blob.minY, blob.maxY, width, pixels);
     }
+}
+
+// free memory from heap allocated array used as Buffer data
+void
+DeleteExternalData(Napi::Env /*&env*/, const uint_fast8_t *finalizeData) {
+    delete[] finalizeData;
 }
--- src/results.h
+++ src/results.h
@@ -29,6 +29,77 @@ SetBoundsResult(const Napi::Env &env, const BoundsResult &boundsResult, Napi::Ar
     resultsJs.Set(index, obj);
 }
 
+inline void
+SetBlobsResult(const Napi::Env &env, const BlobsResult &blobsResult, Napi::Array &resultsJs, const uint_fast32_t index = 0) {
+    Napi::Object obj = Napi::Object::New(env);
+    obj.Set("name", blobsResult.name);
+    obj.Set("percent", blobsResult.percent);
+    obj.Set("minX", blobsResult.minX);
+    obj.Set("maxX", blobsResult.maxX);
+    obj.Set("minY", blobsResult.minY);
+    obj.Set("maxY", blobsResult.maxY);
+    Napi::Array blobsJs = Napi::Array::New(env);
+    uint_fast32_t j = 0;
+    for (const auto &blob : blobsResult.blobs) {
+        if (!blob.flagged) continue;
+        Napi::Object blobJs = Napi::Object::New(env);
+        blobJs.Set("label", blob.label);
+        blobJs.Set("percent", blob.percent);
+        blobJs.Set("minX", blob.minX);
+        blobJs.Set("maxX", blob.maxX);
+        blobJs.Set("minY", blob.minY);
+        blobJs.Set("maxY", blob.maxY);
+        blobsJs.Set(j++, blobJs);
+    }
+    obj.Set("blobs", blobsJs);
+    resultsJs.Set(index, obj);
+}
+
+inline void
+SetGrayPixels(const uint_fast32_t minX, const uint_fast32_t maxX, const uint_fast32_t minY, const uint_fast32_t maxY, const uint_fast32_t width, uint_fast8_t *pixels) {
+    uint_fast8_t *firstPtr = &pixels[minY * width + minX];
+    uint_fast8_t *secondPtr = &pixels[maxY * width + minX];
+    for (uint_fast32_t x = minX; x <= maxX; ++x, ++firstPtr, ++secondPtr) {
+        *firstPtr = 0x80;
+        *secondPtr = 0x80;
+    }
+    firstPtr = &pixels[minY * width + minX + width];
+    secondPtr = &pixels[minY * width + maxX + width];
+    for (uint_fast32_t y = minY, yLimit = maxY - 1; y < yLimit; ++y, firstPtr += width, secondPtr += width) {
+        *firstPtr = 0x80;
+        *secondPtr = 0x80;
+    }
+}
+
+inline void
+SetRgbPixels(const uint_fast32_t minX, const uint_fast32_t maxX, const uint_fast32_t minY, const uint_fast32_t maxY, const uint_fast32_t width, const uint_fast32_t pixDepth, uint_fast8_t *pixels) {
+    uint_fast32_t inc = 1;
+    if (pixDepth > 3) {
+        inc += (pixDepth - 3);
+    }
+    uint_fast8_t *firstPtr = &pixels[minY * width * pixDepth + minX * pixDepth];
+    uint_fast8_t *secondPtr = &pixels[maxY * width * pixDepth + minX * pixDepth];
+    for (uint_fast32_t x = minX; x <= maxX; ++x, firstPtr += inc, secondPtr += inc) {
+        *firstPtr = 0xAF;
+        *(++firstPtr) = 0x33;
+        *(++firstPtr) = 0xFF;
+        *secondPtr = 0xAF;
+        *(++secondPtr) = 0x33;
+        *(++secondPtr) = 0xFF;
+    }
+    inc = width * pixDepth;
+    firstPtr = &pixels[minY * width * pixDepth + minX * pixDepth + width * pixDepth];
+    secondPtr = &pixels[minY * width * pixDepth + maxX * pixDepth + width * pixDepth];
+    for (uint_fast32_t y = minY, yLimit = maxY - 1; y < yLimit; ++y, firstPtr += inc, secondPtr += inc) {
+        *firstPtr = 0xAF;
+        *(firstPtr + 1) = 0x33;
+        *(firstPtr + 2) = 0xFF;
+        *secondPtr = 0xAF;
+        *(secondPtr + 1) = 0x33;
+        *(secondPtr + 2) = 0xFF;
+    }
+}
+
 // all/mask percent to js
 void
 ToJs(const Napi::Env &env, const PercentResult &percentResult, Napi::Array &resultsJs);
@@ -48,6 +119,10 @@ ToJs(const Napi::Env &env, const std::vector<BoundsResult> &boundsResultVec, Nap
 // all/mask blobs to js
 void
 ToJs(const Napi::Env &env, const BlobsResult &blobsResult, Napi::Array &resultsJs);
+
+// regions blobs to js
+void
+ToJs(const Napi::Env &env, const std::vector<BlobsResult> &blobsResultVec, Napi::Array &resultsJs);
 
 // draw bounding box in gray pixels for all/mask
 void
