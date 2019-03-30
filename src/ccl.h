4343bd0d2438061ff58000bfc918ccf6f58bbe3e
Kevin Godell <kevin.godell@gmail.com>
2019-03-30 09:03:41 -0500

    starting to functionalize the recursion

--- src/ccl.h
+++ src/ccl.h
@@ -3,7 +3,16 @@
 
 #include "engine.h"
 #include <cstdint>
-#include <vector>
+
+/*inline uint_fast32_t
+getX(uint_fast32_t *stack, uint_fast32_t stackPointer) {
+    return stack[stackPointer - 3];
+}
+
+inline uint_fast32_t
+getY(uint_fast32_t *stack, uint_fast32_t stackPointer) {
+    return stack[stackPointer - 2];
+}*/
 
 void
 LabelComponent(uint_fast32_t *stack, const Config &config, const Bounds &bounds, int_fast32_t labelNumber, uint_fast32_t x, uint_fast32_t y, int_fast32_t *labels);
