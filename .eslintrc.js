f5ce5c756d0fc7ff4310d589bc18e9e95bd6ea40
Kevin Godell <kevin.godell@gmail.com>
2023-07-30 19:55:48 -0500

    update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
-  ignorePatterns: ['/docs/'],
   env: {
-    es2017: true,
-    es2020: true,
-    es6: true,
+    es2022: true,
     node: true,
   },
   extends: ['plugin:prettier/recommended', 'plugin:markdown/recommended'],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
   plugins: ['prettier', 'markdown'],
   rules: {
     'prettier/prettier': 'error',
     'spaced-comment': ['error', 'always'],
   },
+  ignorePatterns: ['/docs/'],
 };
